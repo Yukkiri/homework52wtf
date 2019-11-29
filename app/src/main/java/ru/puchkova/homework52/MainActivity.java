9b87c6139c70b224b3b4bebabcd5b1810c55be8c
Raccoon <o.sillycoon@gmail.com>
2019-11-29 18:38:54 +0300

    what

--- app/src/main/java/ru/puchkova/homework52/MainActivity.java
+++ app/src/main/java/ru/puchkova/homework52/MainActivity.java
@@ -1,19 +1,28 @@
 package ru.puchkova.homework52;
 
-import androidx.appcompat.app.AppCompatActivity;
+
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
+import androidx.appcompat.widget.Toolbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
     }
 
