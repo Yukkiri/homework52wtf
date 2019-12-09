df4d462b4f6a56375a8d0a10741f100fcbd9e17e
Raccoon <o.sillycoon@gmail.com>
2019-12-09 19:45:09 +0300

    why

--- app/src/main/java/ru/puchkova/homework52/MainActivity.java
+++ app/src/main/java/ru/puchkova/homework52/MainActivity.java
@@ -22,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
 
     }
 
